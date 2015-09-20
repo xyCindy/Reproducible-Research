83b72595a843b305e7d2da9142d516ed75bb9da1
Cindy <xinyuechen213@gmail.com>
2015-09-19 21:32:02 -0400

    week2 quiz complete

---
+++ week2/week2_quiz.R
@@ -0,0 +1,16 @@
+#Week 2 Quiz
+#Question 1
+#---John Gruber
+
+#Question 2
+#---```{r}
+
+#Question 3
+#---Set the 'fig.height' and 'fig.width' options for the code chunk
+
+#Question 4
+# asis means don't postprocess the results,just show the raw results
+#---Set the option results = "asis"
+
+#Question 5
+#---Every time you knit() the R Markdown file, the HTML file will be overwritten
